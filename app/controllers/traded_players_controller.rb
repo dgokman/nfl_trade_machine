8957f875a93af0cfb40017da630be51688665ce6
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-07 10:47:06 -0400

    completed the trade loop

--- app/controllers/traded_players_controller.rb
+++ app/controllers/traded_players_controller.rb
@@ -8,20 +8,17 @@ class TradedPlayersController < ApplicationController
     @traded_player = TradedPlayer.new
   end
 
-  def show
+  def create
+    @teams = Team.all
+    @trade_team = TradeTeam.find(params[:trade_team_id])
+    @trade_player = @trade_team.traded_players.build(trade_players_params)
+    redirect_to trades_path
   end
 
-  # def create
-  #   @trade_team = TradeTeam.all
-  #   @traded_player = current_user.traded_players.build(traded_players_params)
-  #   @traded_player.save
-  #   redirect_to new_trade_team_traded_player
-  # end
-
-  # private
+  private
 
   def trade_players_params
-    params.require(:trade_team).permit(:team_id)
+    params.require(:traded_player).permit(:player_id, :user_id)
   end
 
 end
