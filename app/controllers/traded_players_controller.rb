3dd0d93b4df51300f29ff63295782e60df8ca7ae
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-08 16:44:21 -0400

    added more validations

--- app/controllers/traded_players_controller.rb
+++ app/controllers/traded_players_controller.rb
@@ -17,7 +17,7 @@ class TradedPlayersController < ApplicationController
     player = Player.find(@traded_player.player_id).name
     if @traded_player.save
       redirect_to new_trade_team_traded_player_path(@trade_team),
-        notice: "#{player} has been selected. Select another player or choose a new team"
+        notice: "#{player} has been selected. Select another player or continue"
     else
       render 'new'
     end
