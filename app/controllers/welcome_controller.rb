c13e614acb4ea8caf7438d97f25ba3a5cdd0551d
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-09 13:59:32 -0400

    added styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,10 @@ class WelcomeController < ApplicationController
       @trades = Trade.where(user_id: current_user.id).order(id: :desc)
     end
   end
+
+  def destroy
+    @trades = current_user.trades
+    @trades.destroy
+    redirect_to root_path
+  end
 end
