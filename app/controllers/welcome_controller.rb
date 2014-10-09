fa16452695460261bd15dee6877d367c2366c3d9
Dennis Gokman <dennis.gokman@gmail.com>
2014-10-09 12:14:28 -0400

    trades now in descending order on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     if user_signed_in?
-      @trades = Trade.where(user_id: current_user.id)
+      @trades = Trade.where(user_id: current_user.id).order(id: :desc)
     end
   end
 end
